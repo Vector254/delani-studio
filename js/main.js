e7cfb1863da34f3404993447b8aa875656600aed
Vector254 <ochrist7@gmail.com>
2020-07-24 19:07:58 +0300

    add mailchimp intergration link

--- js/main.js
+++ js/main.js
@@ -34,55 +34,55 @@ $(document).ready(function() {
 
 $(document).ready(function() {
     $("#work1").hover(function() {
-        $(this).animate({opacity:0.6});
+        $(this).animate({opacity:0.4});
         $('#work1text').fadeIn();
     }, function() {
         $(this).animate({opacity:1})
         $('#work1text').fadeOut();  
     });
     $("#work2").hover(function() {
-        $(this).animate({opacity:0.6});
+        $(this).animate({opacity:0.4});
         $('#work2text').fadeIn();
     }, function() {
         $(this).animate({opacity:1})
         $('#work2text').fadeOut();  
     });
     $("#work3").hover(function() {
-        $(this).animate({opacity:0.6});
+        $(this).animate({opacity:0.4});
         $('#work3text').fadeIn();
     }, function() {
         $(this).animate({opacity:1})
         $('#work3text').fadeOut();  
     });
     $("#work4").hover(function() {
-        $(this).animate({opacity:0.6});
+        $(this).animate({opacity:0.4});
         $('#work4text').fadeIn();
     }, function() {
         $(this).animate({opacity:1})
         $('#work4text').fadeOut();  
     });
     $("#work5").hover(function() {
-        $(this).animate({opacity:0.6});
+        $(this).animate({opacity:0.4});
         $('#work5text').fadeIn();
     }, function() {
         $(this).animate({opacity:1})
         $('#work5text').fadeOut();  
     });
     $("#work6").hover(function() {
-        $(this).animate({opacity:0.6});
+        $(this).animate({opacity:0.4});
         $('#work6text').fadeIn();
     }, function() {
         $(this).animate({opacity:1})
         $('#work6text').fadeOut();  
     });
     $("#work7").hover(function() {
-        $(this).animate({opacity:0.6});
+        $(this).animate({opacity:0.4});
         $('#work7text').fadeIn();
     }, function() {
         $(this).animate({opacity:1})
         $('#work7text').fadeOut();  
     });$("#work8").hover(function() {
-        $(this).animate({opacity:0.6});
+        $(this).animate({opacity:0.4});
         $('#work8text').fadeIn();
     }, function() {
         $(this).animate({opacity:1})
